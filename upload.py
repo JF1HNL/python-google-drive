4f963c6dc0e7a9f4b18b2c57c41d4e2df569ef18
JF1HNL <kimossan88@gmail.com>
2021-10-07 23:08:44 +0900

    first commit

---
+++ upload.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import pickle
+import os.path
+import io
+import sys
+import glob
+
+# pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaFileUpload
+
+SCOPES = ['https://www.googleapis.com/auth/drive']
+FOLDER_NAME = 'test'
+os.chdir('files')
+
+files = glob.glob("./*")
+
+
+def main():
+    # OAuth
+    drive = None
+    creds = None
+    if os.path.exists('../token.pickle'):
+        with open('../token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        elif os.path.exists('../client_secret.json'):
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '../client_secret.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('../token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    if creds and creds.valid:
+        drive = build('drive', 'v3', credentials=creds)
+    if not drive: print('Drive auth failed.')
+
+    # Folfer list
+    folders = None
+    if drive: 
+        results = drive.files().list(
+            pageSize=100, 
+            fields='nextPageToken, files(id, name)',
+            q='name="' + FOLDER_NAME + '" and mimeType="application/vnd.google-apps.folder"'
+            ).execute()
+        folders = results.get('files', [])
+        if not folders: print('No folders found.')
+
+    # file upload
+    folder_id = folders[0]["id"] # 複数ある場合を考えない
+    for file in files:
+      file_metadata = {
+          'name': file.replace('./', ''),
+          'parents': [folder_id]
+      }
+      media = MediaFileUpload(
+          file, 
+          mimetype='text/plain', 
+          resumable=True
+      )
+      file = drive.files().create(
+          body=file_metadata, media_body=media, fields='id'
+      ).execute()
+
+if __name__ == '__main__':
+    main()
+    print("upload files")
